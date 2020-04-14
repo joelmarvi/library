8b4225aaaccd13755ee0fe9aa369b52f74202bb9
Jorge Marzola <jorgemarzola@gmail.com>
2020-04-14 15:56:41 +0200

    Add fronted with bootstrap and create render function

--- js/main.js
+++ js/main.js
@@ -1,50 +1,30 @@
 // Array for storing books
-let myLibrary = [];
+let myLibrary = [new Book("The Hobbit", "J.R.R. Tolkien", 295, false), new Book("Freakonomics", "Steven Levitt", 200, true)];
+const books = myLibrary;
+books.forEach((item) => render(item));
 
 // Book object
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
+    this.read = read ? "Yes" : "No";
 }
 
-// Add a new book to the lirary
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+
+function render(book) {
+   const bookList = document.querySelector("#book-list");
+   const bookWrapper = document.createElement("tr");
+   bookWrapper.innerHTML = `
+   <td class="text-center">${book.title}</td>
+   <td class="text-center">${book.author}</td>
+   <td class="text-center">${book.pages}</td>
+   <td class="text-center">${book.read}</td>
+   <td class="text-center"><button class="btn btn-danger btn-sm rounded-circle">X</button></td>` 
+
+   bookList.appendChild(bookWrapper);
 }
 
-function generateTableHead(table, data) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
-}
 
-function generateTable(table, data) {
-    for (let element of data) {
-        let row = table.insertRow();
-        for (key in element) {
-            let cell = row.insertCell();
-            let text = document.createTextNode(element[key]);
-            cell.appendChild(text);
-        }
-    }
-}
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const freakonomics = new Book("Freakonomics", "Steven Levitt", 200, true);
-
-addBookToLibrary(theHobbit);
-addBookToLibrary(freakonomics);
-
-let table = document.querySelector("table");
-let data = Object.keys(myLibrary[0]);
-
-generateTableHead(table, data);
-generateTable(table, myLibrary);
 
