f71f9ca9a2e5b872ee3736dab8e98744ba55fda5
Jorge Marzola <jorgemarzola@gmail.com>
2020-04-14 10:51:16 +0200

    Add generateTableHead and generateTable functions

--- js/main.js
+++ js/main.js
@@ -9,18 +9,31 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
-// Function for displaying Book information
-Book.prototype.info = function() {
-    if (this.isRead == true) {
-        return `${this.title} by ${this.author}, ${this.pages} pages, already read.`
-    } else {
-        return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`
-    }
-}
-
 // Add a new book to the lirary
 function addBookToLibrary(book) {
     myLibrary.push(book);
+}
+
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+function generateTable(table, data) {
+    for (let element of data) {
+        let row = table.insertRow();
+        for (key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
@@ -29,6 +42,9 @@ const freakonomics = new Book("Freakonomics", "Steven Levitt", 200, true);
 addBookToLibrary(theHobbit);
 addBookToLibrary(freakonomics);
 
-console.log(theHobbit.info());
-console.log(freakonomics.info());
-console.table(myLibrary);
+let table = document.querySelector("table");
+let data = Object.keys(myLibrary[0]);
+
+generateTableHead(table, data);
+generateTable(table, myLibrary);
+
